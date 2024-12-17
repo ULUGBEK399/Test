fa62d907efe10da85f070d5531243775a8a57d71
ULUGBEK399 <56226594+ULUGBEK399@users.noreply.github.com>
2024-12-17 14:29:39 +0500

    Update Testbot.py
    version 1.1

--- Testbot.py
+++ Testbot.py
@@ -9,4 +9,9 @@ class CourseAdmin(admin.ModelAdmin):
 
 admin.site.register(Course,CourseAdmin)
 admin.site.register(Subject)
+import telebot
+
+bot = telebot.TeleBot('7743791918:AAFVNhBM4ZWVB1QLg4_IEgPNPO4RCcSJMuE')
+@bot.message_handler(commnds=['start', 'hello', 'main'])
+def main(message):
 
